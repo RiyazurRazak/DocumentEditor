b86aa58e05e47393e4859e7b0d280728b91f5331
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-04-17 17:35:35 +0530

    create doc added load stored doc in quill editor

--- Controllers/EditorController.cs
+++ Controllers/EditorController.cs
@@ -24,7 +24,7 @@ namespace DocumentEditor.Controllers
             Console.WriteLine(id);
             _connection.Open();
             Editor doc = new();
-            string query = $"select * from documents where authorid='{id}'";
+            string query = $"select * from documents where id='{id}'";
             SqlCommand cmd = new SqlCommand(query, _connection);
             SqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
@@ -36,6 +36,7 @@ namespace DocumentEditor.Controllers
             }
             reader.Close();
             _connection.Close();
+            Console.WriteLine(doc.content);
             return View(doc);
         }
 
@@ -72,11 +73,24 @@ namespace DocumentEditor.Controllers
             string id = Request.Form["author"];
             string docId = Request.Form["id"];
             _connection.Open();
-            string query = $"Insert into documents values('{docId}', '{id}', '{data}')";
+            string query = $"update documents set content='{data}' where id='{docId}'";
             SqlCommand cmd = new SqlCommand(query, _connection);
             cmd.ExecuteReader().Close();
             _connection.Close();
-            return RedirectToAction("Index",new {id = id});
+            return RedirectToAction("Index",new {id = docId});
         }
+
+        public IActionResult Create()
+        {
+
+            _connection.Open();
+            string id = Guid.NewGuid().ToString();
+            string query = $"insert into documents values('{id}', '11fa2cb0-59c8-4551-8380-57d534d8529d', '')";
+            SqlCommand cmd = new SqlCommand(query, _connection);
+            cmd.ExecuteReader().Close();
+            _connection.Close();
+            return RedirectToAction("Index", new { id = id });
+        }
+
     }
 }
